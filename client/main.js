a8c79ad0c72ec9c9d9627f5fc6b8487133282fc6
letmegro <nick.kor23@gmail.com>
2022-11-08 21:24:13 -0500

    first page (front end)
    All necessary tags have been added
    and styles.
    JavaScript works and tested with test data.
    Note: the option to create a public/private room should be an option and should be added in the future.

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { addChat } from './publicChats.js'
 
-document.querySelector('#username').innerHTML = `
+//top part/ first part of the page
+document.querySelector("#username").innerHTML = `
       <div>
           <h1>Name</h1> 
       </div>
@@ -8,8 +9,68 @@ document.querySelector('#username').innerHTML = `
           <input type="text" id="UName" name="Chatname" placeholder="Anon123">
       </div>
 `;
-document.getElementById('public-chat').appendChild(addChat(4));
 
 
+//middle part / second part of the page
+document.getElementById("public-chat").appendChild(getButtons(10));
 
-//add a button listener
+function getButtons(Qty){
+    return addChat(Qty);
+}
+
+const buttons = document.getElementsByName("join-public-chat");
+
+var roomCode;
+var userName;
+for(let button of buttons){
+    button.addEventListener("click", (e) => {
+        //gets the name displayed
+        roomCode = document.getElementById(`uniqueCode${button.id}`).innerText;
+        userName = document.getElementById("UName").value;
+        if(userName == ""){
+            userName = "Anon123";
+        }
+        //alert(roomCode);
+    });
+}
+
+//third part of the page / bottom of the page
+document.querySelector("#private-input-buttons").innerHTML = `
+    <div>
+        <button id="create-private-room">Create Private Room</button>
+    </div>
+    <div id="room-code-title">
+        <button id="join-private-room">Join Private Room</button>
+    </div>
+    <div id="room-code-title">
+        <div>
+            <h5>Enter Private Room Code</h5>
+        </div>
+        <div>
+            <input type="text" id="private-code" name="code" placeholder="#######">
+        </div>
+    </div>
+    
+`;
+
+var privateCode;
+
+document.getElementById("join-private-room").addEventListener("click", (e) => {
+    privateCode = document.getElementById("private-code").value;
+    if(privateCode == ""){
+        alert("Please enter a code");
+    }
+    else{
+        userName = document.getElementById("UName").value;
+    }
+    
+});
+
+document.getElementById("create-private-room").addEventListener("click", (e) =>{
+    //request to create a room and redirect the user to that private chatroom instance
+    //below are notes
+    //http redirect tests
+    //location.replace("#")
+    //location.href = "#";
+    //alert("created");
+});
--- client/publicChats.js
+++ client/publicChats.js
@@ -1,17 +1,28 @@
 //chat information will come from the server on request
 //below will be temporary data for testing and placeholding
+//this function is how the buttons would be created 
 export function addChat(Qty){
   var newChats = document.createDocumentFragment();
   for(var i = 0; i < Qty; ++i){
     var addChat = document.createElement('div');
     addChat.className = "chat-display-template";
-    addChat.id = "Room" + i;
+    addChat.id = "div" + i;
     var addBtn = document.createElement("button");
     addBtn.className = "join-public-chat";
+    addBtn.name = "join-public-chat";
+    addBtn.id = i;
     addBtn.innerText = "Join";
+    //could possibly display the public rooms code
+    var uniquecode = document.createElement("p");
+    uniquecode.name = "rCode"+i;
+    uniquecode.id = "uniqueCode"+i;
+    uniquecode.className = "public-chat-name";
+    uniquecode.innerHTML = uniquecode.name;
+    addChat.appendChild(uniquecode);
     addChat.appendChild(addBtn);
     newChats.appendChild(addChat);
   }
+  
   return newChats;
 }
 
