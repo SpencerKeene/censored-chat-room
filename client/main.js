2c936ce20ce8f20dd6eca11f8bdaf72db7958806
letmegro <nick.kor23@gmail.com>
2022-11-13 23:22:39 -0500

    base code for front-end to API
    added some base code for the communication (fetch: GET, POST)
    so that the front end can communicate with the API url and format is needed but the general code is there

--- client/main.js
+++ client/main.js
@@ -11,25 +11,47 @@ document.querySelector("#username").innerHTML = `
 `;
 
 //middle part / second part of the page
-document.getElementById("public-chat").appendChild(getButtons(5));
+document.getElementById("public-chat").appendChild(getButtons()); //getbuttons will fetch the list of objects
 
 function getButtons(Qty){
     return addChat(Qty);
 }
 
 const buttons = document.getElementsByName("join-public-chat");
+const url = "";
 
 var roomCode;
 var userName;
+var chatroomname;
+//join a public chatroom
 for(let button of buttons){
     button.addEventListener("click", (e) => {
         //gets the name displayed
-        roomCode = document.getElementById(`uniqueCode${button.id}`).innerText;
+        roomCode = document.getElementById(`${button.id}`).id;//button holds the rooms id
         userName = document.getElementById("UName").value;
         if(userName == ""){
             userName = "Anon123";
         }
-        //alert(roomCode);
+        chatroomname = document.getElementById(`uniqueCode${button.id}`).innerHTML;
+        //fetch starts below
+        let data = {
+            name: chatroomname,
+            id: roomCode,
+            userName: userName  
+        } //not sure how the user's name will be handled but it will send data in this format for now
+        //header will have the standard UTF-8 as a placeholder until api is done and we know exactly what we'll need
+        let request = new Request(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json; charset=UTF-8'
+            })
+        });
+
+        fetch(request).then((Response) => {
+            //location.replace("#"); //not sure how it is handles yet
+        });
+        //fetch ends here
     });
 }
 
@@ -65,7 +87,7 @@ document.querySelector("#radio-buttons").innerHTML = `
 `;
 
 var privateCode;
-
+//join a prvate chatroom
 document.getElementById("join-private-room").addEventListener("click", (e) => {
     privateCode = document.getElementById("private-code").value;
     if(privateCode == ""){
@@ -74,19 +96,33 @@ document.getElementById("join-private-room").addEventListener("click", (e) => {
     else{
         userName = document.getElementById("UName").value;
     }
-    
+    if(userName == ""){
+        userName = "Anon123";
+    }
+    //fetch starts below
+    let data = {
+        id: privateCode,
+        userName: userName
+    } 
+
+    let request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json; charset=UTF-8'
+        })
+    });
+
+    fetch(request).then((Response) => {
+        //location.replace("#"); //not sure how it is handles yet
+    });
+    //fetch ends here
 });
 
 var roomType = "";
-
+//create a chatroom
 const radioButtons = document.querySelectorAll('input[name="rButton"]');
 document.getElementById("create-chat-room").addEventListener("click", (e) => {
-    //below are notes
-    //http redirect tests
-    //location.replace("#")
-    //location.href = "#";
-    //alert("created");
-
     for(const radioButton of radioButtons){
         if(radioButton.checked){
             roomType = radioButton.value;
@@ -96,8 +132,33 @@ document.getElementById("create-chat-room").addEventListener("click", (e) => {
     if(roomType == ""){
         alert("Please select a room type.");
     }
+    else{
+        userName = document.getElementById("UName").value;
+        if(userName == ""){
+            userName = "Anon123";
+        }
+        //fetch starts below
+        let data = {
+            userName: userName,
+            privacy: roomType
+        } 
+    
+        let request = new Request(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json; charset=UTF-8'
+            })
+        });
+    
+        fetch(request).then((Response) => {
+            //location.replace("#"); //not sure how it is handles yet
+        });
+        //fetch ends here
+    }
+    
 });
 
 document.getElementById("refresh-btn").addEventListener("click", (e) => {
-    //fetch from api
+    document.getElementById("public-chat").replaceChild(getButtons());
 });
--- client/publicChats.js
+++ client/publicChats.js
@@ -1,7 +1,43 @@
 //chat information will come from the server on request
 //below will be temporary data for testing and placeholding
 //this function is how the buttons would be created 
-export function addChat(Qty){
+var Qty = 5;
+var chats;
+//request and reload public chatrooms
+/*export function addChat(){
+
+  fetch('addurl').then((reponse) =>{
+    return response.json(); //returns a response
+  }).then((data) => {
+    chats = data;           //gets the data
+  }).catch((error) => {
+      console.log(error);
+  });
+
+  var newChats = document.createDocumentFragment();
+  for(let chat of chats){ // let object in list of objects
+    var addChat = document.createElement('div');
+    addChat.className = "chat-display-template";
+    addChat.id = "div" + chat.id; //"div" + object.id
+    var addBtn = document.createElement("button");
+    addBtn.className = "join-public-chat";
+    addBtn.name = "join-public-chat";
+    addBtn.id = chat.id; //object.id
+    addBtn.innerText = "Join";
+    //could possibly display the public rooms code
+    var uniquecode = document.createElement("p");
+    uniquecode.name = chat.name;
+    uniquecode.id = "uniqueCode"+chat.id;
+    uniquecode.className = "public-chat-name";
+    uniquecode.innerHTML = uniquecode.name;
+    addChat.appendChild(uniquecode);
+    addChat.appendChild(addBtn);
+    newChats.appendChild(addChat);
+  }
+  
+  return newChats;
+}*/
+export function addChat(){ //commented out copy of the code above in case of fuck up delete when rdy
   var newChats = document.createDocumentFragment();
   for(var i = 0; i < Qty; ++i){
     var addChat = document.createElement('div');
