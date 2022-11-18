cbd04e8beff9e4cf752085e6255ec1bf7ac06e56
Connor Della-Savia <conndellasavia@gmail.com>
2022-11-17 19:19:04 -0500

    add websocket
    allow server to pass messages to all connected clients

--- websocket_server/server.js
+++ websocket_server/server.js
@@ -1,5 +1,7 @@
+const express = require('express')
 const http = require('http')
-const app = require('./app')
+const app = express()
+const ws = require('ws')
 
 // const port = process.env.port
 // if (!port) throw Error('Port number must be provided')
@@ -8,4 +10,24 @@ const port = 5000
 
 const server = http.createServer(app)
 
+const wsServer = new ws.Server({ noServer: true })
+wsServer.on('connection', socket => {
+
+  socket.on('message', message => 
+    console.log(JSON.parse(message.toString()))
+    )
+  
+  socket.on('message', message =>
+    wsServer.clients.forEach(function each(client) {
+        if(client != socket){
+            client.send(message, {binary: false})
+        } 
+    }))
+})
 server.listen(port)
+
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request)
+    })
+})
