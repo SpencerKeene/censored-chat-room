742a93dae2c2c234d159a6f001e33b311d265029
SpencerKeene <spencerakeene@gmail.com>
2022-11-22 21:37:42 -0500

    add endpoint to get a single chatroom

--- ajax_server/api/routes/chatrooms.js
+++ ajax_server/api/routes/chatrooms.js
@@ -5,6 +5,8 @@ const ChatroomsController = require('../controllers/chatrooms')
 
 router.get('/', ChatroomsController.get_all_chatrooms)
 
+router.get('/:chatroomId', ChatroomsController.get_one_chatroom)
+
 router.post('/', ChatroomsController.create_chatroom)
 
 module.exports = router
